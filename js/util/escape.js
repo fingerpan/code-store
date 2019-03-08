4f896afb4536a53872e105e7160ca7bf79bdc380
youngpan <youngpan2014@outlook.com>
2019-03-08 17:15:03 +0800

    修改目录

---
+++ js/util/escape.js
@@ -0,0 +1,28 @@
+
+
+
+const escapeCharMap = {
+    '&': "&amp;",
+    '<': "&lt;",
+    '>': "&gt;",
+    ' ': "&nbsp;",
+    "'": "&#39;",
+    '"': "&quot;",
+}
+
+/**
+ * 将字符串中特殊字符替换为转义字符，防御xss攻击
+ * @example 
+ * // returns "&lt;script&gt;&lt;/script&gt;"
+ * escape('<script></script>');
+ * 
+ * // returns '&nbsp;&nbsp;'
+ * escape('  ');
+ * 
+ * @param {String} char 需要转移的字符串
+ * @returns {String} 转义后的字符串
+ */
+function escape(char){
+    if(char.length === 0) return "";
+    return Object.keys(escapeCharMap).reduce((newStr, element) => newStr.replace(new RegExp(element, 'g'), escapeCharMap[element]), String(char));
+}
